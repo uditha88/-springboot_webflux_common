6f4aa9c587e13807f3ebc7a6066e1a1ae87c2e4a
uditha-perera <uditha.p@integritas.net>
2026-03-17 13:47:01 +0530

    Changes webflux common  add keycloak auth2

--- commit.sh
+++ commit.sh
@@ -1,15 +1,15 @@
 git add .; 
-git commit -m "Changes webflux common"; 
+git commit -m "Changes webflux common  add keycloak auth2"; 
 git push -u origin HEAD;
 
 cd ../webflux
 git add .; 
-git commit -m "Changes webflux"; 
+git commit -m "Changes webflux add keycloak auth2"; 
 git push -u origin HEAD;
 
 cd ../webflux_2
 git add .; 
-git commit -m "Changes webflux_2"; 
+git commit -m "Changes webflux_2 add keycloak auth2"; 
 git push -u origin HEAD;
 
 
