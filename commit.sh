4d43cc0d88cf37cb1e01925344c5efafa51e1069
uditha-perera <uditha.p@integritas.net>
2026-03-24 17:48:16 +0530

    Changes webflux common  add jaeger dist. tracing

--- commit.sh
+++ commit.sh
@@ -1,15 +1,15 @@
 git add .; 
-git commit -m "Changes webflux common  add keycloak auth2"; 
+git commit -m "Changes webflux common  add jaeger dist. tracing"; 
 git push -u origin HEAD;
 
 cd ../webflux
 git add .; 
-git commit -m "Changes webflux add keycloak auth2"; 
+git commit -m "Changes webflux add jaeger dist. tracing"; 
 git push -u origin HEAD;
 
 cd ../webflux_2
 git add .; 
-git commit -m "Changes webflux_2 add keycloak auth2"; 
+git commit -m "Changes webflux_2 add jaeger dist. tracing"; 
 git push -u origin HEAD;
 
 
